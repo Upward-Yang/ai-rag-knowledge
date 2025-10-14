2cb05eb8b05994d7b5a4662043b32feac1a676f4
yangjian <525919113@qq.com>
2025-10-14 17:03:16 +0800

    5 上传文件，读取内容分割解析内容，上传到pgsql向量数据库，然后通过读取pgsql存储的内容，传入大模型进行问答对话，输出结果

--- xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/config/OllamaConfig.java
+++ xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/config/OllamaConfig.java
@@ -1,10 +1,16 @@
 package cn.bugstack.xfg.dev.tech.config;
 
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.OllamaEmbeddingClient;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * @Auther: yangjian
@@ -22,5 +28,39 @@ public class OllamaConfig {
     @Bean
     public OllamaChatClient ollamaChatClient(OllamaApi ollamaApi) {
         return new OllamaChatClient(ollamaApi);
+    }
+
+    /**
+     * 用于切割文本的操作
+     * @return
+     */
+    @Bean
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+    /**
+     * 把知识库缓存到内存
+     * @param ollamaApi
+     * @return
+     */
+    @Bean
+    public SimpleVectorStore simpleVectorStore(OllamaApi ollamaApi) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new SimpleVectorStore(embeddingClient);
+    }
+
+    /**
+     * 把知识库通过 JdbcTemplate 也就是 pgsql 操作，存储到向量库
+     * @param ollamaApi
+     * @param jdbcTemplate
+     * @return
+     */
+    @Bean
+    public PgVectorStore pgVectorStore(OllamaApi ollamaApi, JdbcTemplate jdbcTemplate) {
+        OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
+        embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
+        return new PgVectorStore(jdbcTemplate, embeddingClient);
     }
 }
